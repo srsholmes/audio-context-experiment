33f90a646e31f6fcd1b398ee4524b237172b1872
srsholmes <srsholmes@gmail.com>
2015-08-16 21:46:58 +0100

    some data visualiser

--- src/app.js
+++ src/app.js
@@ -57,20 +57,12 @@ let soundCloudAudio = () => {
 
 	      //Anaylse that shit
 	      analyser = audioCtx.createAnalyser();
-	      analyser.fftSize = 256; // see - there is that 'fft' thing. 
-	      let dataArray = new Uint8Array(analyser.frequencyBinCount); // Uint8Array should be the same length as the frequencyBinCount 
+	      analyser.fftSize = 256;
+	      let dataArray = new Uint8Array(128); // Uint8Array should be the same length as the frequencyBinCount 
+	      audio.connect(analyser);
+	      analyser.connect(audioCtx.destination);
 
-	      // var source = audioCtx.createMediaElementSource(player); // this is where we hook up the <audio> element
-	      // source.connect(analyser);
-	      // analyser.connect(audioCtx.destination);
-
-	      var sampleAudioStream = function() {
-	      	console.log('sampleAudioStream');
-	      	console.log(audio);
-	      	console.log(dataArray);
-	          // This closure is where the magic happens. Because it gets called with setInterval below, it continuously samples the audio data
-	          // and updates the streamData and volume properties. This the SoundCouldAudioSource function can be passed to a visualization routine and 
-	          // continue to give real-time data on the audio stream.
+	      let sampleAudioStream = function() {
 	          analyser.getByteFrequencyData(dataArray);
 	          // calculate an overall volume value
 	          var total = 0;
@@ -78,71 +70,32 @@ let soundCloudAudio = () => {
 	              total += dataArray[i];
 	          }
 	          dataArray.volume = total;
+	         	// console.log(dataArray);
 	      };
-	      setInterval(sampleAudioStream, 2000);
+	      setInterval(sampleAudioStream, 20);
+
+	      let bin;
+	      let canvas = document.getElementById('canvas');
+	      let context = canvas.getContext("2d");
+
+        let draw = function() {
+        	console.log(audio);
+        	var bin;
+            for(bin = 0; bin < dataArray.length; bin ++) {
+            	console.log(audio);
+                // do something with each value. Here's a simple example
+                var val = dataArray[bin];
+                var red = val;
+                var green = 255 - val;
+                var blue = val / 2; 
+                context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+                context.fillRect(bin * 2, 0, 2, 200);
+            }          
+        };
+          setInterval(draw, 20);
 
 	    });
 		};
 		xhr.send();
 	});	
 }();
-
-// var SoundCloudAudioSource = function(audioElement) {
-//     var player = document.getElementById(audioElement);
-//     var self = this;
-//     console.log(this);
-//     var analyser;
-//     var audioCtx = new (window.AudioContext || window.webkitAudioContext); // this is because it's not been standardised accross browsers yet.
-//     analyser = audioCtx.createAnalyser();
-//     analyser.fftSize = 256; // see - there is that 'fft' thing. 
-//     var source = audioCtx.createMediaElementSource(player); // this is where we hook up the <audio> element
-//     source.connect(analyser);
-//     analyser.connect(audioCtx.destination);
-//     var sampleAudioStream = function() {
-//     	console.log('sampleAudioStream');
-//         // This closure is where the magic happens. Because it gets called with setInterval below, it continuously samples the audio data
-//         // and updates the streamData and volume properties. This the SoundCouldAudioSource function can be passed to a visualization routine and 
-//         // continue to give real-time data on the audio stream.
-//         analyser.getByteFrequencyData(self.streamData);
-//         console.log(self.streamData);
-//         // calculate an overall volume value
-//         var total = 0;
-//         for (var i = 0; i < 80; i++) { // get the volume from the first 80 bins, else it gets too loud with treble
-//             total += self.streamData[i];
-//         }
-//         self.volume = total;
-//     };
-//     setInterval(sampleAudioStream, 2000); // 
-//     // public properties and methods
-//     this.volume = 0;
-//     this.streamData = new Uint8Array(128); // This just means we will have 128 "bins" (always half the analyzer.fftsize value), each containing a number between 0 and 255. 
-//     this.playStream = function(streamUrl) {
-//         // get the input stream from the audio element
-//         player.setAttribute('src', streamUrl);
-//         player.play();
-//     }
-// };
-
-// var audioSource = new SoundCloudAudioSource('player');
-// var canvasElement = document.getElementById('canvas');
-// var context = canvasElement.getContext("2d");
-
-// var draw = function() {
-// 	var bin;
-//     // you can then access all the frequency and volume data
-//     // and use it to draw whatever you like on your canvas
-//     for(bin = 0; bin < audioSource.streamData.length; bin ++) {
-//         // do something with each value. Here's a simple example
-//         var val = audioSource.streamData[bin];
-//         var red = val;
-//         var green = 255 - val;
-//         var blue = val / 2; 
-//         context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-//         context.fillRect(bin * 2, 0, 2, 200);
-//         // use lines and shapes to draw to the canvas is various ways. Use your imagination!
-//     }
-//     requestAnimationFrame(draw);
-// };
-
-// audioSource.playStream('https://soundcloud.com/heldeepradio/oliver-heldens-heldeep-radio-034');
-// draw();
