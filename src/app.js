6a20263c936ab8670cff01bdad9fc568c07b583d
srsholmes <srsholmes@gmail.com>
2015-08-17 20:42:47 +0100

    refactor with promises

--- src/app.js
+++ src/app.js
@@ -15,202 +15,91 @@ React.render(<App/>, document.querySelector('div[app]'));
 
 const CLIENT_ID = `9c470b57005415330972a0b5cca327e2`;
 
-let audio;
-let analyser;
-let audioCtx = new AudioContext();
-let xhr = new XMLHttpRequest();
-let player = document.getElementById('player');
-
 SC.initialize({
   client_id: CLIENT_ID
 });
 
+let d = document;
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+let play = d.querySelector('.play');
+let stop = d.querySelector('.stop');
+let canvas = d.getElementById('canvas');
+let context = canvas.getContext("2d");
+
+let analyser;
+let bin;
+let source;
+
+
 let getStreamUrl = (url) => {
-	var apiURL = `http://api.soundcloud.com/resolve.json?url=${url}&client_id=${CLIENT_ID}`;
+	let apiURL = `http://api.soundcloud.com/resolve.json?url=${url}&client_id=${CLIENT_ID}`;
 	return fetch(apiURL)
   .then(status)
   .then(json)
   .then((data) => { 
-  	console.log(data);
-  	return data.stream_url
-  });
-};
-
-let getStreamData = (streamURL) => {
-	streamURL = `${streamURL}?client_id=${CLIENT_ID}`;
-	return fetch(streamURL)
-	.then(status)
-	.then((data) => { 
-  	console.log(data);
-  	return data
+  	let streamURL = `${data.stream_url}?client_id=${CLIENT_ID}`;
+  	return streamURL
   });
 };
 
 let streamURL = getStreamUrl('https://soundcloud.com/matzelu/summer-is-comming');
 
-let soundCloudAudio = () => {
-	streamURL
-	.then(getStreamData)
-	.then((data) => {
-		console.log('fetch loooad');
-		console.log(data);
-		console.log(response);
-    audio = audioCtx.createBufferSource();
-    audioCtx.decodeAudioData(xhr.response, (buffer) => {
-      audio.buffer = buffer;
-      audio.connect(audioCtx.destination);
-      audio.start();
 
-      //Anaylse that shit
-      analyser = audioCtx.createAnalyser();
-      analyser.fftSize = 256;
-      let dataArray = new Uint8Array(128);
-      audio.connect(analyser);
-      analyser.connect(audioCtx.destination);
-
-      let sampleAudioStream = function() {
-          analyser.getByteFrequencyData(dataArray);
-          var total = 0;
-          for (var i = 0; i < 80; i++) {
-              total += dataArray[i];
-          }
-          dataArray.volume = total;
-         	console.log(dataArray);
-      };
-      setInterval(sampleAudioStream, 20000);
-
-      let bin;
-      let canvas = document.getElementById('canvas');
-      let context = canvas.getContext("2d");
-
-      let draw = function() {
-        for(bin = 0; bin < dataArray.length; bin ++) {
-            var val = dataArray[bin];
-            var red = val;
-            var green = 255 - val;
-            var blue = val / 2; 
-            context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-            context.fillRect(bin * 2, 0, 2, 200);
-        }          
-      };
-      setInterval(draw, 20);
-
+let getData = (url) => {
+  source = audioCtx.createBufferSource();
+  source.start(0);
+  play.setAttribute('disabled', 'disabled');
+  let myRequest = new Request(url);
+  fetch(myRequest).then((response) => {
+    response.arrayBuffer().then((buffer) => {
+      audioCtx.decodeAudioData(buffer, (decodedData) => {
+        source.buffer = decodedData;
+        source.connect(audioCtx.destination); 
+        analyse();  
+      });
     });
-	});
+  });
+};
 
+//Anaylse that shit
+let analyse = () => {
+  analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 256;
+  let dataArray = new Uint8Array(128);
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
 
-	// streamURL.then((streamURL) => {
-	// 	streamURL = `${streamURL}?client_id=${CLIENT_ID}`;
-	// 	xhr.open('GET', streamURL, true);
-	// 	xhr.responseType = 'arraybuffer';
-	// 	xhr.onload = function() {
-	// 		console.log('xhr loooad');
-	//     audio = audioCtx.createBufferSource();
-	//     audioCtx.decodeAudioData(xhr.response, (buffer) => {
-	//       audio.buffer = buffer;
-	//       audio.connect(audioCtx.destination);
-	//       audio.start();
+  let sampleAudioStream = function() {
+    analyser.getByteFrequencyData(dataArray);
+    let total = 0;
+    for (let i = 0; i < 80; i++) {
+        total += dataArray[i];
+    }
+    dataArray.volume = total;
+  };
+  setInterval(sampleAudioStream, 20);
 
-	//       //Anaylse that shit
-	//       analyser = audioCtx.createAnalyser();
-	//       analyser.fftSize = 256;
-	//       let dataArray = new Uint8Array(128);
-	//       audio.connect(analyser);
-	//       analyser.connect(audioCtx.destination);
+  //Canvas visual stuff
+  let draw = () => {
+    for(bin = 0; bin < dataArray.length; bin ++) {
+        let val = dataArray[bin];
+        let red = val;
+        let green = 255 - val;
+        let blue = val / 2; 
+        context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+        context.fillRect(bin * 2, 0, 2, 200);
+    }          
+  };
+  setInterval(draw, 20);
+}
 
-	//       let sampleAudioStream = function() {
-	//           analyser.getByteFrequencyData(dataArray);
-	//           var total = 0;
-	//           for (var i = 0; i < 80; i++) {
-	//               total += dataArray[i];
-	//           }
-	//           dataArray.volume = total;
-	//          	console.log(dataArray);
-	//       };
-	//       setInterval(sampleAudioStream, 20000);
+play.onclick = () => {
+  streamURL
+	.then((url) => getData(url));
+}
 
-	//       let bin;
-	//       let canvas = document.getElementById('canvas');
-	//       let context = canvas.getContext("2d");
-
- //        let draw = function() {
- //          for(bin = 0; bin < dataArray.length; bin ++) {
- //              var val = dataArray[bin];
- //              var red = val;
- //              var green = 255 - val;
- //              var blue = val / 2; 
- //              context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
- //              context.fillRect(bin * 2, 0, 2, 200);
- //          }          
- //        };
- //        setInterval(draw, 20);
-
-	//     });
-	// 	};
-	// 	xhr.send();
-	// });	
-}();
-
-
-// let soundCloudAudio = () => {
-// 	// let streamURL = "http://api.soundcloud.com/tracks/293/stream?client_id=9c470b57005415330972a0b5cca327e2";
-// 	let streamURL2 = getStreamUrl('https://soundcloud.com/matzelu/summer-is-comming');
-// 	// let streamURL3 = "https://api.soundcloud.com/tracks/218176109/stream?client_id=9c470b57005415330972a0b5cca327e2";
-
-// 	let audio;
-// 	let analyser;
-// 	let audioCtx = new AudioContext();
-// 	let xhr = new XMLHttpRequest();
-// 	let player = document.getElementById('player'); 
-
-// 	streamURL2.then((streamURL) => {
-// 		streamURL = `${streamURL}?client_id=${CLIENT_ID}`;
-// 		xhr.open('GET', streamURL, true);
-// 		xhr.responseType = 'arraybuffer';
-// 		xhr.onload = function() {
-// 			console.log('xhr loooad');
-// 	    audio = audioCtx.createBufferSource();
-// 	    audioCtx.decodeAudioData(xhr.response, (buffer) => {
-// 	      audio.buffer = buffer;
-// 	      audio.connect(audioCtx.destination);
-// 	      audio.start();
-
-// 	      //Anaylse that shit
-// 	      analyser = audioCtx.createAnalyser();
-// 	      analyser.fftSize = 256;
-// 	      let dataArray = new Uint8Array(128);
-// 	      audio.connect(analyser);
-// 	      analyser.connect(audioCtx.destination);
-
-// 	      let sampleAudioStream = function() {
-// 	          analyser.getByteFrequencyData(dataArray);
-// 	          var total = 0;
-// 	          for (var i = 0; i < 80; i++) {
-// 	              total += dataArray[i];
-// 	          }
-// 	          dataArray.volume = total;
-// 	         	console.log(dataArray);
-// 	      };
-// 	      setInterval(sampleAudioStream, 20000);
-
-// 	      let bin;
-// 	      let canvas = document.getElementById('canvas');
-// 	      let context = canvas.getContext("2d");
-
-//         let draw = function() {
-//           for(bin = 0; bin < dataArray.length; bin ++) {
-//               var val = dataArray[bin];
-//               var red = val;
-//               var green = 255 - val;
-//               var blue = val / 2; 
-//               context.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-//               context.fillRect(bin * 2, 0, 2, 200);
-//           }          
-//         };
-//         setInterval(draw, 20);
-
-// 	    });
-// 		};
-// 		xhr.send();
-// 	});	
-// }();
+stop.onclick = () => {
+  source.stop(0);
+  play.removeAttribute('disabled');
+}
